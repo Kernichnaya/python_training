c056f846ddd4c78a41adecea4a6ef08c6639f483
Lyubov Kernichnaya <kernithnaya@gmail.com>
2021-10-05 03:33:40 +0700

    Выделен класс для управления сессиями

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.group import Group
+from model.contact import Contact
 from fixture.application import Application
 
 @pytest.fixture
@@ -10,18 +11,17 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
-    app.open_groups_page()
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="Group1", header="Group1", footer="Group1"))
-    app.logout()
+    app.session.logout()
 
 def test_empty_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 def test_create_add_new(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.contact_add(Contact(firstname="lv", middlname="kr", lastname="lv2", nickname="kr2", title="Title",
                             company="Company2", address="elizarovix", homephone="97-52-5", mobile="+79293713057",
                             workphone="89893585085", faxphone="89811115085", email="klobastov@gmal.com",
@@ -29,6 +29,6 @@ def test_create_add_new(app):
                             email3="rtg@gmal.com", homepage="dfgdfg", bday="14", bmonth="September",
                             byear="2000", aday="15", amonth="November", ayear="1997", address2="Adress",
                             phone2="852451285", notes="23"))
-    app.logout()
+    app.session.logout()
 
 
