e6baf368841e7f277e59bcc281a78e043aff211d
Lyubov Kernichnaya <kernithnaya@gmail.com>
2021-11-02 03:48:26 +0700

    Задание 14

--- test/test_comparison_contacts.py
+++ test/test_comparison_contacts.py
@@ -5,7 +5,6 @@ def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.address == contact_from_edit_page.address
 
 
 def test_phones_on_contact_view_page(app):
@@ -16,18 +15,15 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.mobile == contact_from_edit_page.mobile
     assert contact_from_view_page.faxphone == contact_from_edit_page.faxphone
 
-
 def test_emails_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
-
 def test_address_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.address == contact_from_edit_page.address
-
 
 def test_name_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
@@ -35,17 +31,14 @@ def test_name_on_home_page(app):
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
 
-
 def clear(s):
     return re.sub("[() -]", "", s)
-
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                        [contact.homephone, contact.mobile, contact.workphone, contact.phone2]))))
-
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join([contact.email, contact.email2, contact.email3])
