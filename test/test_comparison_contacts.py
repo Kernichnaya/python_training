dce5cd5af31444997d0089e34c1298cc2ee90407
Lyubov Kernichnaya <kernithnaya@gmail.com>
2021-11-02 03:47:26 +0700

    Задание 14

--- test/test_comparison_contacts.py
+++ test/test_comparison_contacts.py
@@ -17,6 +17,25 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.faxphone == contact_from_edit_page.faxphone
 
 
+def test_emails_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+
+
+def test_address_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.address == contact_from_edit_page.address
+
+
+def test_name_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+
+
 def clear(s):
     return re.sub("[() -]", "", s)
 
@@ -28,13 +47,5 @@ def merge_phones_like_on_home_page(contact):
                                        [contact.homephone, contact.mobile, contact.workphone, contact.phone2]))))
 
 
-def test_emails_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-
-
 def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
+    return "\n".join([contact.email, contact.email2, contact.email3])
