c8177e6567c0560ff34f843d9935b481e99cd391
Lyubov Kernichnaya <kernithnaya@gmail.com>
2021-11-06 01:31:33 +0700

    Задание №14 Исправления

--- test/test_comparison_contacts.py
+++ test/test_comparison_contacts.py
@@ -41,4 +41,6 @@ def merge_phones_like_on_home_page(contact):
                                        [contact.homephone, contact.mobile, contact.workphone, contact.phone2]))))
 
 def merge_emails_like_on_home_page(contact):
-    return "\n".join([contact.email, contact.email2, contact.email3])
+    return "\n".join(filter(lambda x: x != "",
+                            filter(lambda x: x is not None,
+                                   [contact.email, contact.email2, contact.email3])))
