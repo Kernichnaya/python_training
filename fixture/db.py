f8886f20f919a56e542c565aa6e1586af91da1a3
Lyubov Kernichnaya <kernithnaya@gmail.com>
2021-11-14 05:41:38 +0700

    Задание №21

--- fixture/db.py
+++ fixture/db.py
@@ -2,7 +2,7 @@
 
 import pymysql.cursors
 from model.group import Group
-
+from model.contact import Contact
 
 class DbFixture:
 
@@ -11,7 +11,7 @@ class DbFixture:
         self.name = name
         self.user = user
         self.password = password
-        self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
         self.connection.autocommit(True)
 
     def get_group_list(self):
@@ -22,6 +22,33 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list_all(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, home, mobile, work, phone2, "
+                           "email, email2, email3, address from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname, home, mobile, work, phone2, email, email2, email3, address) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, homephone=home, mobile=mobile,
+                                    workphone=work,
+                                    phone2=phone2, email=email, email2=email2, email3=email3, address=address))
         finally:
             cursor.close()
         return list
--- generator/contact.py
+++ generator/contact.py
@@ -33,12 +33,13 @@ def random_number(maxlen):
 
 
 testdatacontact = [Contact(firstname="", middlename="", lastname="", nickname="", company="", address="",
-                           homephone="", mobile="", faxphone="")] + [
+                           homephone="", mobile="", faxphone="", phone2="", workphone="")] + [
                       Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
                               lastname=random_string("lastname", 10),
                               nickname=random_string("nickname", 10), company=random_string("company", 10),
                               address=random_string("address", 10),
-                              homephone=random_number(11), mobile=random_number(11), faxphone=random_number(11))
+                              homephone=random_number(11), mobile=random_number(11), faxphone=random_number(11),
+                              workphone=random_number(7), phone2=random_number(11))
                       for i in range(n)
                   ]
 
